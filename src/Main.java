444d7ce4079703a45bedc14a9bb4acaaf7356036
Kateryna Nuzhna <katranchik21@gmail.com>
2024-10-31 03:38:03 +0100

    init commit mit commentars

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-
+/*
+Есть два списка одинаковой длины с числами.
+Написать функцию, которая вернет список с элементами True или False,
+где значение на i-том месте зависит от того, равны ли элементы двух списков под номером i.
+ Например, {1, 2, 3, 4} и {5, 2, 3, 8} вернет {False, True, True, False}
+ */
 public class Main {
     public static void main(String[] args) {
         //створимо списки з числами
